c17a96ef67545a8aa47f4069c68e4c34eee031e1
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-05-01 19:17:57 -0300

    up

---
+++ js/ex2/script.js
@@ -0,0 +1,8 @@
+let pessoa = { nome: 'Maria',
+               idade: 18};
+
+console.log(pessoa["nome"]);
+
+let listadesejos = ["Carro", "PC", "Guitarra"];
+
+console.log("Quer ganhar um = " + listadesejos[1]);
